3c09cb6079394b157cf7b0c8dc463fb656e8838e
Trevor Johannessen <91626575+Trevor-Johannessen@users.noreply.github.com>
2022-11-02 11:29:32 -0400

    Revert "Revert "Commented out print statements""
    This reverts commit 785c8a4f2dd533443389ad35b0e4dc3f942e5db8.

--- colon.lua
+++ colon.lua
@@ -93,7 +93,7 @@ function parse(text, line_num)
 
 	-- turn arguments into parameters for the objects
 	for term in text do
-			print("term = ", term)
+			--print("term = ", term)
 			local equals_pos = string.find(term, "=")
 			args[string.sub(term, 0, equals_pos-1)] = string.gsub(string.sub(term, equals_pos+1), string.char(9), ",") -- args[var_name] = var_value
 			--print("arg = ", string.sub(term, 0, equals_pos-1))
@@ -114,7 +114,7 @@ function parse(text, line_num)
 		
 	-- if regular object
 	elseif object_type == "tag" then
-		print("args[tag] = ", args["tag"])
+		--print("args[tag] = ", args["tag"])
 		tags[args["tag"]] = args
 		return -1
 	elseif object_type == "background" then
@@ -129,7 +129,7 @@ function parse(text, line_num)
 		if found_tag then
 			for k, v in next, tags[found_tag] do 
 				-- add all tag attributes to object here
-				print("arg[", k, "] = ", args[k])
+				--print("arg[", k, "] = ", args[k])
 				if args[k] == nil then
 					args[k] = v
 				end
@@ -164,7 +164,7 @@ function initalize()
 	apis = fs.list("/colon/colon_apis/colon_objects/")
 	
 	for i=1, table.getn(apis) do
-		print("apis[".. i .. "] = ", apis[i])
+		--print("apis[".. i .. "] = ", apis[i])
 		if not fs.isDir(apis[i]) then
 			os.loadAPI("/colon/colon_apis/colon_objects/" .. apis[i])
 			object_types[string.sub(apis[i], 1, -5)] = true
@@ -179,7 +179,7 @@ end
 
 
 function construct_when(args)
-	print("construct = ", string.sub(args.command, 2, -2))
+	--print("construct = ", string.sub(args.command, 2, -2))
 	--os.sleep(2)
 	when[args.name] = string.sub(args.command, 2, -2)
 end
@@ -266,7 +266,7 @@ end
 
 function check_when_statements(name)
 	for k, v in next, when do
-		print(k, " ", v)
+		--print(k, " ", v)
 		if k == name then
 			--term.clear()
 			--print("Interpreting line: ", string.gsub(v, "\\", ""))
@@ -281,7 +281,7 @@ end
 -- HELPER FUNCTIONS
 function printarr(arr, substr)
    for i in pairs(arr) do
-      print("arr[" .. i .. "] = ", arr[i])
+      --print("arr[" .. i .. "] = ", arr[i])
 	  if substr then printarr(arr[i]) end
    end
 end
