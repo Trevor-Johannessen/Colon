3571c7329893b997b54aa8b1950bd08c10d071e6
Trevor Johannessen <trevor.johannessen@gmail.com>
2023-03-14 22:47:30 -0400

    Rewrote draw function
    Rewrote draw function to precompute the colored string and store each in a table. This is to allow for scrolling implementation in the future.

--- colon_apis/colon_objects/text.lua
+++ colon_apis/colon_objects/text.lua
@@ -19,10 +19,14 @@ function create(args)
 	text.sticky = args.sticky or false
 	text.screen_width, text.screen_height = term.getSize()
 	text.length = string.len(text.text)
-	text.finish = tonumber(args.finish) or text.screen_width
-	text.full_lines = math.floor(text.length / (text.finish - text.x))
-	text.partial_lines = monus(1, text.length - (text.finish - text.x)*text.full_lines)
-	text.height = text.full_lines + text.partial_lines + 1
+	text.width = tonumber(args.width) or text.screen_width
+	text.height = tonumber(args.height) or nil
+	text.scrollPos = 0
+	text.scrollable = args.scrollable or true
+	text.lineCount = 0	
+	text.strTable = {}
+	text.clrTable = {}
+	text.bgdTable = {}
 	
 	function text:draw(x_offset, y_offset)
 		local save_cursor = {term.getCursorPos()}
@@ -39,35 +43,49 @@ function create(args)
 			term.setTextColor(text:convertColor(text.color, "int"))
 			term.setBackgroundColor(text:convertColor(text.background, "int"))
 			str, colorString, backgroundString = text:parseColor(text.text)
-			local newY = text.y
-			while str:len() > 0 do
-				term.setCursorPos(text.x+x_offset, newY-y_offset)
-				-- need to find the amount to increment the cursor by each loop (should be the amount of characters displayed)
-				local endPointer = text.finish
-				local offset = 1
-				local line = str:sub(1, endPointer-text.x)
-				if str:len() > text.finish - text.x and line:find(' ') then
-					endPointer = line:find('[^ ]*$')+1
-				end
-				if(str:sub(endPointer-text.x+1,endPointer-text.x+1) == " ") then offset = 2 end
-				--print("Last char = " .. str:sub(endPointer-text.x, endPointer - text.x) .. "     ")
-				term.blit(str:sub(1,endPointer-text.x), colorString:sub(1,endPointer-text.x), backgroundString:sub(1,endPointer-text.x))
-				str=str:sub(endPointer-text.x+offset)
-				colorString=colorString:sub(endPointer-text.x+offset)
-				backgroundString=backgroundString:sub(endPointer-text.x+offset)
+			local newY = 1
+			while text.height >= newY and #text.strTable >= newY do
+				term.setCursorPos(text.x+x_offset, text.y+newY-y_offset-1)
+				term.blit(text.strTable[newY], text.clrTable[newY], text.bgdTable[newY])
 				newY = newY + 1
 			end
-			
 			term.setCursorPos(save_cursor[1], save_cursor[2])
 			term.setTextColor(save_text)
 			term.setBackgroundColor(save_background)
 		end
 	end
 	
+	function text:update(args)
+		scroll:draw(obj_args["x_offset"], obj_args["y_offset"])
+	end
+	
+	function text:initalize() -- dry render to populate text tables
+		local str, colorString, backgroundString = text:parseColor(text.text)
+		while str:len() > 0 do
+			-- need to find the amount to increment the cursor by each loop (should be the amount of characters displayed)
+			local endPointer = text.width+text.x
+			local offset = 1
+			local line = str:sub(1, endPointer-text.x)
+			if str:len() > text.width - text.x and line:find(' ') then
+				endPointer = line:find('[^ ]*$')+1
+			end
+			if(str:sub(endPointer-text.x+1,endPointer-text.x+1) == " ") then offset = 2 end
+			table.insert(text.strTable, str:sub(1,endPointer-text.x))
+			table.insert(text.clrTable, colorString:sub(1,endPointer-text.x))
+			table.insert(text.bgdTable, backgroundString:sub(1, endPointer-text.x))
+			str=str:sub(endPointer-text.x+offset)
+			colorString=colorString:sub(endPointer-text.x+offset)
+			backgroundString=backgroundString:sub(endPointer-text.x+offset)
+			text.lineCount = text.lineCount + 1
+		end
+		text.height = text.hegiht or text.lineCount
+	end
+
 	-- correction to clean inputs
 	if sharedFunctions then
 		sharedFunctions.corrections(text)
 	end
+	text:initalize()
 	
 	return text
 end
