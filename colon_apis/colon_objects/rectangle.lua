6e13e805396960508e9aa7bc812db6ba343ca8a5
Trevor Johannessen <trevor.johannessen@gmail.com>
2023-05-25 21:59:27 -0400

    Added ability to add character to rectangle

--- colon_apis/colon_objects/rectangle.lua
+++ colon_apis/colon_objects/rectangle.lua
@@ -12,7 +12,12 @@ function create(args)
 	rectangle.type = "rectangle"
 	rectangle.name = args.name
 	rectangle.hollow = args.hollow or false
+	rectangle.character=args.char or " "
+	rectangle.characterColor = args.charColor or colors.white
 	
+	if rectangle.characterColor then rectangle.characterColor = rectangle:convertColor(rectangle.characterColor, "int") end
+	if rectangle.character:len() > 1 then rectangle:error("Character must be a single character long.") end
+
 	function rectangle:draw(x_offset, y_offset)
 		local oldx, oldy = term.getCursorPos()
 		local save_background = term.getBackgroundColor()	
@@ -20,16 +25,17 @@ function create(args)
 			y_offset = 0 
 			x_offset = 0
 		end	 
+		term.setTextColor(rectangle.characterColor)
 		term.setBackgroundColor(rectangle.color)
 		for i=0, rectangle.height-1 do
 		
 			term.setCursorPos(rectangle.x+x_offset, rectangle.y-y_offset+i)
 			if rectangle.hollow and i ~= 0 and i ~= rectangle.height-1 then
-				io.write(" ")
+				io.write(rectangle.character)
 				term.setCursorPos(rectangle.x+x_offset+rectangle.width-1, rectangle.y-y_offset+i)
-				io.write(" ")
+				io.write(rectangle.character)
 			else
-				io.write(string.rep(" ", rectangle.width))
+				io.write(string.rep(rectangle.character, rectangle.width))
 			end
 		end	
 		term.setBackgroundColor(save_background)
