d6e671506af1809cb03f42fad42f2116cfe76ea7
Trevor Johannessen <trevor.johannessen@gmail.com>
2023-05-28 23:31:58 -0400

    Changed menu selected to be placed at position 1 on initalization. Menu also now only shows selected box when focused.

--- colon_apis/colon_objects/menu.lua
+++ colon_apis/colon_objects/menu.lua
@@ -2,7 +2,7 @@ template = require("colon_apis/colon_objects/template")
 button = require("colon_apis/colon_objects/button")
 
 function create(args)
-	local menu = template.create()
+	local menu = template.create(args)
 	
 	menu.x = tonumber(args.x) or 1
 	menu.y = tonumber(args.y) or 1
@@ -25,12 +25,13 @@ function create(args)
     menu.selectedColor = menu:correctColor(args.selectedColor) or colors.white
     menu.selectedTextColor = menu:correctColor(args.selectedTextColor) or colors.black
     menu.page = args.page or "Unknown"
-    menu.selectedSaveState={}
     menu.offset=0
+    menu.retain = args.retainFocus == "true"
 
     menu.hardcodeHeight = 1
 
     function menu:generateButtons()
+        menu.buttons = {}
         local offset = 0
         for k, text in next, menu.options do
             table.insert(menu.buttons, menu:createButton(text, {x=menu.x,y=menu.y+offset}))
@@ -69,6 +70,7 @@ function create(args)
     end
 
     function menu:parseOptString()
+        menu.options = {}
         local optItr = string.gmatch(menu.optString, "([^;]*);*")
         for str in optItr do
             table.insert(menu.options, str)
@@ -81,6 +83,7 @@ function create(args)
     end
 
     function menu:draw(x_offset, y_offset)
+        if menu.hidden then return end
         for k, v in next, menu.buttons do
             if k > menu.offset and k <= menu.height+menu.offset then
                 v:draw(x_offset, menu:calculateYOffset(y_offset))
@@ -88,53 +91,62 @@ function create(args)
         end
     end
 
-    function menu:focus()
-        menu.focused=true
-    end
-
-    function menu:setSelected(newSelector, context)
-        if menu.selected ~= 0 then -- restoration policy
+    function menu:restoreSelected(index)
+        if type(menu.selectedSaveState) ~= "nil" then -- restoration policy
             menu.buttons[menu.selected].sprite:setImage(menu.selectedSaveState.color, true)
             menu.buttons[menu.selected].backgroundColor = menu.selectedSaveState.textColorBackground
             menu.buttons[menu.selected].textColor = menu.selectedSaveState.textColor
         end
-        -- select new button
+    end
+
+    function menu:saveSelected()
         menu.selectedSaveState = {
-            color=menu.buttons[newSelector].spriteFile,
-            textColor=menu.buttons[newSelector].textColor,
-            textColorBackground=menu.buttons[newSelector].backgroundColor
+            color=menu.buttons[menu.selected].spriteFile,
+            textColor=menu.buttons[menu.selected].textColor,
+            textColorBackground=menu.buttons[menu.selected].backgroundColor
         }
-        menu.selected = newSelector
+    end
+
+    function menu:colorSelected()
         menu.buttons[menu.selected].sprite:setImage(menu.selectedColor, true)
         menu.buttons[menu.selected].textColor = menu.selectedTextColor
         menu.buttons[menu.selected].backgroundColor = menu.selectedColor
-        
-        menu:draw(context.x_offset, context.y_offset)
+    end
+
+    function menu:setSelected(newSelector, context)
+        menu:restoreSelected()
+        menu.selected = newSelector
+        menu:saveSelected()
+        menu:colorSelected()
+        if context then 
+            menu:draw(context.x_offset, context.y_offset)
+        end
     end
 
 	function menu:update(obj_args)
+        if menu.hidden then return end
 		-- for clicking on the menu
         local return_args = {}
+        if menu.focused then return_args = {"nobubble"} end
 		if obj_args["mouse_x"] and obj_args["mouse_y"] then 
-			if menu.sticky then y_offset = 0 end
+			if menu.sticky then obj_args.y_offset = 0 end
 			local hit = menu:isClickInMenu(obj_args)
 			if hit then
                 if menu.focused == false and obj_args["event"] == "mouse_up" then -- clicking menu for the first time
-                    menu.colon.log("Toggled Menu")
-                    menu.focused = true
+                    menu:setFocus(true)
+                    menu.buttons[menu.selected]:draw(obj_args.x_offset, obj_args.y_offset)
                     if inColon then
                         colon.scrollLock(true)
                     end
                 elseif menu.focused then -- menu is already selected
                     for k, button in next, menu.buttons do
                         local x = button:update(obj_args)
-                        if x then menu:setSelected(k, obj_args) table.insert(return_args, "when"); return_args["whenArgs"] = {menu.options[menu.selected]} end
+                        if x then menu:setSelected(k, obj_args); return_args = menu:submit(return_args) end
                     end
                 end
                 table.insert(return_args, "scroll")
-			elseif obj_args.event == "mouse_up" then
-                menu.colon.log("Untoggled menu")
-				menu.focused = false
+			elseif obj_args.event == "mouse_up" and menu.focused then
+				menu:setFocus(false)
 				colon.scrollLock(false)
 			end
 		-- for navigating the menu
@@ -143,24 +155,54 @@ function create(args)
 				if menu.selected > 1 then
                     if menu.selected - menu.offset == 0 then menu.offset = menu.offset-1 end
                     menu:setSelected(menu.selected-1, obj_args)
-                    menu.colon.log("Selected = " .. menu.selected)
+                    return_args["whenArgs"] = {"selected", menu.selected}
                 end
 			elseif obj_args["event"] == "key" and obj_args["event_id"] == 264 then -- down arrow
                 if menu.selected ~= menu.optCount then
                     if menu.selected >= menu.height+menu.offset then menu.offset = menu.offset+1 end
                     menu:setSelected(menu.selected+1, obj_args)
-                    menu.colon.log("Selected = " .. menu.selected)
+                    return_args["whenArgs"] = {"selected", menu.selected}
                 end
             elseif obj_args["event"] == "key" and obj_args["event_id"] == 257 then -- enter key
-                table.insert(return_args, "when")
-                return_args["whenArgs"] = {menu.options[menu.selected]}
+                return_args = menu:submit(return_args)
             end
 		end
         return return_args
-	end
+	end 
+
+    function menu:submit(return_args)
+        if not menu.retain then menu:setFocus(false) end
+        table.insert(return_args, "when")
+        return_args["whenArgs"] = {"pressed", menu.options[menu.selected]}
+        return return_args
+    end
+
+    function menu:setOptions(newOptString)
+        menu.optString = newOptString
+        menu:parseOptString()
+        menu:generateButtons()
+    end
+
+    function menu:focus()
+        menu:setFocus(true)
+    end
+
+    function menu:unfocus()
+        menu:setFocus(false)
+    end
+
+    function menu:setFocus(new)
+        menu.focused = new
+        if new then
+            menu:colorSelected()
+        else
+            menu:restoreSelected()
+        end
+    end
 
     menu:parseOptString()
     menu:generateButtons()
+    if menu.optCount > 0 then menu.selected = 1; menu:saveSelected() end
 
 	return menu
 end
