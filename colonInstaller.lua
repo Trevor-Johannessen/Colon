098d7b33145749b05849cf65b81884ab94a6297f
Trevor Johannessen <trevor.johannessen@gmail.com>
2023-06-18 00:04:13 -0400

    Updated installer

--- colonInstaller.lua
+++ colonInstaller.lua
@@ -1,37 +1,55 @@
 print("Running installer v0.0.1")
 objects = {
- 'action', 'api', 'button', 'gif', 'jumpcut', 'loadbar', 
- 'run', 'scroll', 'sprite', 'swipeLeft', 'swipeRight', 
- 'template', 'text', 'textbox', 'reactangle',
+ 'action', 'api', 'block', 'button', 'call', 'dropdown', 'fade', 'gif', 'hook', 'jumpcut', 'loadbar', 'menu', 'rectangle',
+ 'run', 'scroll', 'slider', 'slot', 'sprite', 'swipeLeft', 'swipeRight', 'switch', 
+ 'template', 'text', 'textbox', 'time'
 }
+
+function writeFile(path, content)
+    local file, msg = fs.open(path, 'w')
+    if not file then
+        print("File could not be written: " .. msg)
+        print("Uninstalling...")
+        uninstall()
+        error("Colon could not be installed: " .. msg)
+    end
+    file.write(content.readAll())
+    file.close()
+end
+
+function uninstall()
+    for i,obj in next, objects do
+        fs.delete(string.format('/colon/colon_apis/colon_objects/%s.lua', obj))
+    end
+    fs.delete("/colon/README.md")
+    fs.delete("/colon/colon.lua")
+    fs.delete("/colon/colonrunner.lua")
+    fs.delete("/colon/colon_apis/colon_objects")
+    fs.delete("/colon/colon_apis")
+    fs.delete("/colon")
+    print("Colon uninstalled!")
+end
+
 -- download and move object files
 for i,obj in next, objects do
     print(string.format("Getting %s.lua...", obj))
     local req = http.get(string.format('https://raw.githubusercontent.com/Trevor-Johannessen/markup/markup-oo/colon_apis/colon_objects/%s.lua', obj))
-    local file = fs.open(string.format('/colon/colon_apis/colon_objects/%s.lua', obj), 'w')
-    file.write(req.readAll())
-    file.close()
+    writeFile(string.format('/colon/colon_apis/colon_objects/%s.lua', obj), req)
     print("Done.")
 end
 -- README
 print("Getting README.md...")
 req = http.get("https://raw.githubusercontent.com/Trevor-Johannessen/markup/markup-oo/README.md")
-file = fs.open("/colon/README.md", 'w')
-file.write(req.readAll())
-file.close()
+writeFile("/colon/README.md", req)
 print("Done.")
 -- colon interpreter
 print("Getting colon.lua...")
 req = http.get("https://raw.githubusercontent.com/Trevor-Johannessen/markup/markup-oo/colon.lua")
-file = fs.open("/colon/colon.lua", 'w')
-file.write(req.readAll())
-file.close()
+writeFile("/colon/colon.lua", req)
 print("Done.")
 -- colon runner
 print("Getting colonrunner.lua...")
 req = http.get("https://raw.githubusercontent.com/Trevor-Johannessen/markup/markup-oo/colonrunner.lua")
-file = fs.open("/colon/colonrunner.lua", 'w')
-file.write(req.readAll())
-file.close()
+writeFile("/colon/colonrunner.lua", req)
 print("Done.")
 print("Installation Complete!")
