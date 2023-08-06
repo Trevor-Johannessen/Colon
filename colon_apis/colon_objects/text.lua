7e27a414ef81343971d1e25401fee9b0625319c3
Trevor Johannessen <trevor.johannessen@gmail.com>
2023-08-06 00:53:04 -0400

    Added feature list and basic arguments.

--- colon_apis/colon_objects/text.lua
+++ colon_apis/colon_objects/text.lua
@@ -2,8 +2,27 @@ template = require("colon_apis/colon_objects/template")
 
 function create(args)
 	local text = template.create(args)
-	
-	function text:draw(x_offset, y_offset)	
+	block=block:coords(args)
+    block=block:dim(args)
+    block=block:essentials(args)
+
+	--[[
+		Features:
+			Display Text
+			Give text color
+			Give text background color
+			Allow text to be added/deleted (Re-renders the whole text)
+			Render a blinking cursor at a specific position (Without re-rendering the whole text)
+			Enable highlighting text with mouse drag (Without re-rendering the whole text)
+			Enable drag highlighting to highlight text which may be off screen.
+			Utilize word-wrap
+			Change size of text boundaries
+			Print a text background box
+			Scroll if text is too large for boundary
+			Support hyperlinks (Trigger when statements)
+	]]
+
+	function text:draw(x_offset, y_offset)
 	end
 	
 	function text:update(args)
